594beda476787a5b2b973578bb3b6dc2d6954483
Nikhilesh Verhwani <nikhileshverhwani@gmail.com>
2023-03-19 23:34:03 +0530

    Remove unused codes

--- main.py
+++ main.py
@@ -31,14 +31,18 @@ def parse_args_and_config():
     
     gid = human_gdrive_ids[edit_type][0]
     model_path = os.path.join('checkpoint', human_gdrive_ids[edit_type][1])
-    dl = GoogleDrive_Dowonloader(True)
-    dl.ensure_file_exists(gid, model_path)
+    # dl = GoogleDrive_Dowonloader(True)
+    isExist = os.path.exists(model_path)
+    if not isExist:
+        raise ValueError
+    # dl.ensure_file_exists(gid, model_path)
 
     t_0 = int(model_path.split('_t')[-1].replace('.pth',''))
     print(f'return step t_0: {t_0}')
     exp_dir = f"runs/MANI_{img_path.split('/')[-1]}_align{align_face}"
     os.makedirs(exp_dir, exist_ok=True)
     args_dic = {
+    'edit_one_image' : True,
     'config': 'celeba.yml', 
     't_0': t_0, 
     'n_inv_step': int(n_inv_step), 
@@ -74,25 +78,6 @@ def parse_args_and_config():
     os.makedirs('checkpoint', exist_ok=True)
     os.makedirs('precomputed', exist_ok=True)
     os.makedirs('runs', exist_ok=True)
-    # os.makedirs(args.exp, exist_ok=True)
-    # args.image_folder = os.path.join(args.exp, 'image_samples')
-    # if not os.path.exists(args.image_folder):
-    #     os.makedirs(args.image_folder)
-    # else:
-    #     overwrite = False
-    #     if args.ni:
-    #         overwrite = True
-    #     else:
-    #         response = input("Image folder already exists. Overwrite? (Y/N)")
-    #         if response.upper() == 'Y':
-    #             overwrite = True
-
-    #     if overwrite:
-    #         # shutil.rmtree(args.image_folder)
-    #         os.makedirs(args.image_folder, exist_ok=True)
-    #     else:
-    #         print("Output image folder exists. Program halted.")
-    #         sys.exit(0)
 
     # add device
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
@@ -125,7 +110,6 @@ def main():
     args, config = parse_args_and_config()
     print(">" * 80)
     logging.info("Exp instance id = {}".format(os.getpid()))
-    logging.info("Exp comment = {}".format(args.comment))
     logging.info("Config =")
     print("<" * 80)
 
