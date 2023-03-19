8af5f5e8c026873bff7e23ba49480143aa72c4e9
Nikhilesh Verhwani <nikhileshverhwani@gmail.com>
2023-03-19 22:45:12 +0530

    Fixed Seed

--- main.py
+++ main.py
@@ -100,8 +100,8 @@ def parse_args_and_config():
     new_config.device = device
 
     # set random seed
-    torch.manual_seed(args.seed)
-    np.random.seed(args.seed)
+    # torch.manual_seed(args.seed)
+    # np.random.seed(args.seed)
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(args.seed)
 
